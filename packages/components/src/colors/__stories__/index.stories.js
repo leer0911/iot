c543e1bb52e6c60cc5f29aa84af385855dc12cdb
LE <445220716@qq.com>
2019-08-16 18:49:33 +0800

    Update index.stories.js

--- packages/components/src/colors/__stories__/index.stories.js
+++ packages/components/src/colors/__stories__/index.stories.js
@@ -1,7 +1,7 @@
 import { storiesOf } from '@storybook/react';
 import React from 'react';
 import README from './README.md';
-import Demo from './demo';
+import Demo from './Demo';
 
 storiesOf('Doc', module)
   .addParameters({
