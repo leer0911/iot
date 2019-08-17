978c72586369f0e31b1e5f2e4b3e5148bdf3f1ac
le <445220716@qq.com>
2019-08-17 12:57:35 +0800

    update demo

--- packages/components/src/colors/__stories__/Demo.js
+++ packages/components/src/colors/__stories__/Demo.js
@@ -6,13 +6,13 @@ import { getContrastRatio } from '../../theme';
 const demo = () => {
   const cont = Object.keys(colors).map(type => {
     return (
-      <Box p={2}>
+      <Box p={2} key={type}>
         <Box p={2} fontWeight="600" fontSize={24}>
           {type}
         </Box>
         {Object.keys(colors[type]).map(shade => {
           return (
-            <Box bgcolor={colors[type][shade]} p={2}>
+            <Box bgcolor={colors[type][shade]} p={2} key={shade}>
               <Box
                 color={
                   getContrastRatio(colors[type][shade], '#333') >= 3
