475acd52ee1779640c942ad906fa46dda77bed55
le <445220716@qq.com>
2019-09-06 00:30:13 +0800

    add mi app

---
+++ packages/apps/mi/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import PropTypes from 'prop-types';
+
+import { Button } from '@iot/components';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Button variant="contained">Test</Button>
+      </header>
+    </div>
+  );
+}
+
+App.propTypes = {
+  component: PropTypes.elementType,
+};
+
+export default App;
