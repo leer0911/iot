948ef7bb1fff43e9b4b8b910a4199155345964fa
le <445220716@qq.com>
2019-09-07 12:58:13 +0800

    add CssBaseline

---
+++ packages/components/src/other/CssBaseline/CssBaseline.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Global } from '@emotion/core';
+import { useTheme } from '../../theme';
+
+function CssBaseline(props) {
+  const { children = null } = props;
+  const theme = useTheme();
+
+  return (
+    <Global
+      styles={{
+        html: {
+          WebkitFontSmoothing: 'antialiased',
+          MozOsxFontSmoothing: 'grayscale',
+          boxSizing: 'border-box',
+        },
+        '*, *::before, *::after': {
+          boxSizing: 'inherit',
+        },
+        'strong, b': {
+          fontWeight: 'bolder',
+        },
+        body: {
+          margin: 0,
+          color: theme.palette.text.primary,
+          backgroundColor: theme.palette.background.default,
+          ...theme.typography.body2,
+        },
+      }}
+    >
+      {children}
+    </Global>
+  );
+}
+
+CssBaseline.propTypes = {
+  children: PropTypes.node,
+};
+
+export default CssBaseline;
---
+++ packages/components/src/other/CssBaseline/index.d.ts
@@ -0,0 +1,7 @@
+export interface CssBaselineProps {
+  children?: React.ReactElement;
+}
+
+declare const CssBaseline: React.ComponentType<CssBaselineProps>;
+
+export default CssBaseline;
