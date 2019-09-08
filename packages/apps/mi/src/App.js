66400837ca89b37669aa70baf7016e923d372a95
le <445220716@qq.com>
2019-09-08 23:32:38 +0800

    merge

--- packages/apps/mi/src/App.js
+++ packages/apps/mi/src/App.js
@@ -11,7 +11,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit 2222<code>src/App.js</code> and save to reload.
         </p>
         <Button variant="contained">Test</Button>
       </header>
