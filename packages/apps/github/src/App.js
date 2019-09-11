6c36c1caff2aaf1e65c6ab081ab6bde1c68e7921
le <445220716@qq.com>
2019-09-11 21:31:50 +0800

    add animate router

--- packages/apps/github/src/App.js
+++ packages/apps/github/src/App.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { useTransition, animated } from 'react-spring';
 import { CssBaseline } from '@iot/components';
 import { PrivateRoute } from './route';
 import { GlobalStoreContext, useStore } from './store';
 import { User, Home, News, Profile, NoMatch } from './route';
 import { useInterceptors } from './utils';
+
+const AnimatRoute = ({ location }) => {
+  const transitions = useTransition(location, location => location.pathname, {
+    from: { opacity: 0, transform: 'translate3d(100%,0,0)' },
+    enter: { opacity: 1, transform: 'translate3d(0%,0,0)' },
+    leave: { opacity: 0, transform: 'translate3d(-50%,0,0)' },
+  });
+  return transitions.map(({ item, props, key }) => (
+    <animated.div key={key} style={{ width: '100vw', height: '100vh', ...props }}>
+      <Switch location={location}>
+        <Route path="/:user(login)" component={User} />
+        <PrivateRoute exact path="/" component={Home} />
+        <PrivateRoute path="/news" component={News} />
+        <PrivateRoute path="/profile" component={Profile} />
+        <Route component={NoMatch} />
+      </Switch>
+    </animated.div>
+  ));
+};
 
 const App = () => {
   const store = useStore();
@@ -16,17 +36,7 @@ const App = () => {
     <GlobalStoreContext.Provider value={store}>
       <CssBaseline />
       <BrowserRouter>
-        <Route
-          render={({ location }) => (
-            <Switch location={location}>
-              <Route path="/:user(login)" component={User} />
-              <PrivateRoute exact path="/" component={Home} />
-              <PrivateRoute path="/news" component={News} />
-              <PrivateRoute path="/profile" component={Profile} />
-              <Route component={NoMatch} />
-            </Switch>
-          )}
-        />
+        <Route component={AnimatRoute} />
       </BrowserRouter>
     </GlobalStoreContext.Provider>
   );
